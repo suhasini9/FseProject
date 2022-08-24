deb5d880aa5cc2279be8b3e9427680f3c88298d1
Handson <suhasinipandu18@gmail.com>
2022-08-24 10:28:08 +0530

    committed changes

---
+++ env.js
@@ -0,0 +1,7 @@
+(function (window) {
+  window["env"] = window["env"] || {};
+
+  // Environment variables
+  window["env"]["API_IP"] = "15.206.151.248";
+  window["env"]["debug"] = true;
+})(this);
